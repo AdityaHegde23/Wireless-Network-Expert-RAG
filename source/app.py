903195e37a27c9b69419612d47c7fb2b30a88c93
Aditya Hegde <ahegd005@ucr.edu>
2024-12-07 02:53:39 -0800

    -- added css

--- source/app.py
+++ source/app.py
@@ -2,13 +2,17 @@ import random
 import gradio as gr
 
 def spec_insight(message, history):
+    print(message)
     return "Wireless network"
+
+custom_css = "./custom_chat.css"
 
 app = gr.ChatInterface(
     spec_insight, 
     type="messages", 
     autofocus=False,
-    title="SpecInsight: AI Driven Wireless Expert"
+    title="SpecInsight: AI Driven Wireless Expert",
+    css=custom_css
 )
 if __name__ == "__main__":
     app.launch()
